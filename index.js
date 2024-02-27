3d6f3971fd312d77730963588f95a13e03e957ef
EduardoCole <eduortza@gmail.com>
2024-02-27 10:41:19 +0000

    instalación de paquetes locales

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cron = require("node-cron");
 
 require("dotenv").config();
 
