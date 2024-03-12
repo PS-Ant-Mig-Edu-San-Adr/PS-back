c6acc483d4a241d29fa9701e4d4063d3c11b3697
EduardoCole <eduortza@gmail.com>
2024-03-12 09:24:09 +0000

    eliminado ID del usuario

--- models/userModel.js
+++ models/userModel.js
@@ -30,10 +30,9 @@ const userSchema = new Schema(
         timeZone: { type: String, enum: Object.values(TimeZone), required: false },
         preferredLanguage: { type: String, enum: Object.values(PreferredLanguage), required: false },
         notificationSettings: { type: String, enum: Object.values(NotificationConfig), required: false },
-        avatar: { type: String, required: false },
+        avatar: { type: String, required: false, default: "https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?w=1380&t=st=1709673019~exp=1709673619~hmac=9d504ec5aa5c1cd54b6b670aa754df8dd5c68099f78b9fd2241712c0eabd41f0" },
         calendar: { type: mongoose.Schema.Types.ObjectId, ref: 'calendarModel', required: true },
         groups: { type: Array, required: false },
-        ID: { type: String, required: false },
         tags: { type: Array, required: false },
     },
     { collection: "users", id: true }
